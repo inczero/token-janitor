14e34061d600f88e6ce060c35f178686c3ca04a2
Incze Roland <incze_roland@yahoo.com>
2023-05-09 11:39:48 +0200

    remove todos

--- main.go
+++ main.go
@@ -3,11 +3,3 @@ package main
 func main() {
 
 }
-
-// TODOs:
-// 1. Create GitHub repository
-// 2. Create Basic Go files (main+mods)
-// 3. Create Dockerfile (multistage build, build stage and different container for the concrete app)
-// 4. Create Helm chart for Cronjob
-// 5. Create build script
-// 6. Create deployment script
