f16eb45d9fd13c6153ff534bc9984e62e77b037d
Incze Roland <incze_roland@yahoo.com>
2023-05-12 01:33:23 +0200

    clean up main function

--- main.go
+++ main.go
@@ -1,41 +1,21 @@
 package main
 
 import (
-	"context"
-	firebase "firebase.google.com/go/v4"
 	"fmt"
-	"google.golang.org/api/option"
+	"github.com/inczero/token-janitor/internal/firebase"
 	"log"
 )
 
 const (
-	FirebasePathToKey   = "path/to/file"
-	FirebaseDatabaseURL = "dburl"
+	FirebasePathToKey   = "path/to/key"
+	FirebaseDatabaseURL = "db-url"
 )
 
 func main() {
-	cont := context.Background()
+	fmt.Println("Starting token-janitor...")
 
-	conf := &firebase.Config{
-		DatabaseURL: FirebaseDatabaseURL,
+	client, initErr := firebase.InitClient(FirebaseDatabaseURL, make([]byte, 0))
+	if initErr != nil {
+		log.Fatalln(initErr)
 	}
-
-	opt := option.WithCredentialsFile(FirebasePathToKey)
-
-	app, newErr := firebase.NewApp(cont, conf, opt)
-	if newErr != nil {
-		log.Fatal(newErr)
-	}
-
-	client, dbErr := app.Database(cont)
-	if dbErr != nil {
-		log.Fatalln("Error initializing database client:", dbErr)
-	}
-
-	ref := client.NewRef("/ref/path")
-	var data map[string]interface{}
-	if err := ref.Get(cont, &data); err != nil {
-		log.Fatalln("Error reading from database:", err)
-	}
-	fmt.Println(data)
 }
