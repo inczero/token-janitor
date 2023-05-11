efbc116ad92f6dacd7788ecef063c11dd2d13a3e
Incze Roland <incze_roland@yahoo.com>
2023-05-11 12:15:14 +0200

    add ref getter sample

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	firebase "firebase.google.com/go/v4"
+	"fmt"
+	"google.golang.org/api/option"
+	"log"
+)
+
+const (
+	FirebasePathToKey   = "path/to/file"
+	FirebaseDatabaseURL = "dburl"
+)
 
 func main() {
-	fmt.Println("Test")
+	cont := context.Background()
+
+	conf := &firebase.Config{
+		DatabaseURL: FirebaseDatabaseURL,
+	}
+
+	opt := option.WithCredentialsFile(FirebasePathToKey)
+
+	app, newErr := firebase.NewApp(cont, conf, opt)
+	if newErr != nil {
+		log.Fatal(newErr)
+	}
+
+	client, dbErr := app.Database(cont)
+	if dbErr != nil {
+		log.Fatalln("Error initializing database client:", dbErr)
+	}
+
+	ref := client.NewRef("/ref/path")
+	var data map[string]interface{}
+	if err := ref.Get(cont, &data); err != nil {
+		log.Fatalln("Error reading from database:", err)
+	}
+	fmt.Println(data)
 }
