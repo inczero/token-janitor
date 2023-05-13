56876c41cf546aef23d0245451dc7ec77ebff0b2
Incze Roland <incze_roland@yahoo.com>
2023-05-13 02:00:05 +0200

    add separator around the image name

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,9 @@
 IMAGE_NAME="token-janitor"
 TAG=$(date +"%Y%m%d-%H%M%S")
 
+echo "######################################"
 echo "Building $IMAGE_NAME:$TAG..."
+echo "######################################"
+echo
 
 docker build -t "$IMAGE_NAME":"$TAG" .
